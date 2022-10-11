a0ebdbc5efca9fb850b16034b390a0202c765281
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-10-11 06:56:54 +0000

    s

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -25,7 +25,13 @@ if (x=="Y"):
     a19=input ("Enter a  Plural Noun")
     a20=input ("Enter a Number")
     a21=input ("Enter a  Plural Noun")
+file=open("./f1.txt")
+# print(file)
+b=file.read()
+# print (b)
 
+print()
+print(b.format(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21))
 
     
 
